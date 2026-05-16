2a5ae5317f0ef008d1172d6aaafc95fbd6641fc3
Colin Saldanha <colin.saldanha@gmail.com>
2026-05-16 13:51:35 -0400

    Simplify Tile

--- src/main/java/com/cks/tetris/model/Tile.java
+++ src/main/java/com/cks/tetris/model/Tile.java
@@ -1,8 +1,4 @@
 package com.cks.tetris.model;
 
-public record Tile(int x, int y, Color color) {
-
-    public Tile(Point p, Color color) {
-        this(p.x, p.y, color);
-    }
+public record Tile(Color color) {
 }
