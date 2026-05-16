e75c7575187094bc907d863147a93d7b6b61de81
Colin Saldanha <colin.saldanha@gmail.com>
2026-05-16 13:56:11 -0400

    Create BlockService

---
+++ src/test/java/com/cks/tetris/service/BlockServiceTest.java
@@ -0,0 +1,34 @@
+package com.cks.tetris.service;
+
+import com.cks.tetris.math.RotationMatrix;
+import com.cks.tetris.model.Block;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class BlockServiceTest {
+
+    @InjectMocks
+    BlockService service;
+
+    @Nested
+    class RotateBlockTest {
+        @Test
+        void whenCalled() {
+            Block original = mock(Block.class);
+            Block rotated = mock(Block.class);
+            when(original.rotate(RotationMatrix.CLOCK_WISE_90)).thenReturn(rotated);
+
+            Block actual = service.rotateBlock(original, RotationMatrix.CLOCK_WISE_90);
+
+            assertThat(actual).isSameAs(rotated);
+            verify(original).rotate(RotationMatrix.CLOCK_WISE_90);
+        }
+    }
+}
