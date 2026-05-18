d3bbbcf5a26f0987a66c581645d55088d7b68112
Colin Saldanha <colin.saldanha@gmail.com>
2026-05-18 09:19:48 -0400

    Create UIConfig
    Contains Spring beans specific to UI.

--- src/main/java/com/cks/tetris/config/AppConfig.java
+++ src/main/java/com/cks/tetris/config/AppConfig.java
@@ -2,15 +2,7 @@ package com.cks.tetris.config;
 
 import org.springframework.context.annotation.Configuration;
 
-import javax.swing.*;
-
 @Configuration
 public class AppConfig {
 
-    public AppConfig() {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ignored) {
-        }
-    }
 }
