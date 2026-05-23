df9a09b4bb87ec0d55da9121c475d45794741e4f
Colin Saldanha <colin.saldanha@gmail.com>
2026-05-23 14:10:02 -0400

    Update ScorePanel display method

--- src/main/java/com/cks/tetris/ui/ScorePanel.java
+++ src/main/java/com/cks/tetris/ui/ScorePanel.java
@@ -17,13 +17,17 @@ public class ScorePanel extends JPanel {
     @Autowired
     public ScorePanel(Font informationFont) {
         super(new GridBagLayout());
-        scoreLabel = new JLabel("Score: 0\tLines: 0");
+        scoreLabel = new JLabel(buildDisplayText(new Score(0, 0)));
         scoreLabel.setFont(informationFont);
         add(scoreLabel, new GBC(0, 0).setInsets(10));
     }
 
     public void setScore(Score score) {
-        this.scoreLabel.setText("Score: " + score.total() + "\tLines: " + score.lines());
+        this.scoreLabel.setText(buildDisplayText(score));
+    }
+
+    private String buildDisplayText(Score score) {
+        return String.format("Score: %d   Lines: %d   Level: %d", score.total(), score.lines(), score.level());
     }
 
 }
