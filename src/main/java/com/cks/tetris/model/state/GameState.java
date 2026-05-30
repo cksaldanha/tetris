0c841ee547d1a038e4e38d9942ae0e0389c1c46a
Colin Saldanha <colin.saldanha@gmail.com>
2026-05-29 20:23:48 -0400

    Add GameState builder

--- src/main/java/com/cks/tetris/model/state/GameState.java
+++ src/main/java/com/cks/tetris/model/state/GameState.java
@@ -4,4 +4,53 @@ import com.cks.tetris.model.Board;
 
 public record GameState(Board board, Score score, boolean paused) {
 
+    public GameState(Builder builder) {
+        this(builder.board, builder.score, builder.paused);
+    }
+
+    public Builder mutate() {
+        return new Builder(this);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Board board;
+        private Score score;
+        private boolean paused;
+
+        protected Builder() {
+        }
+
+        protected Builder(GameState original) {
+            this.board = original.board;
+            this.score = original.score;
+            this.paused = original.paused;
+        }
+
+        public Builder board(Board board) {
+            this.board = board;
+            return self();
+        }
+
+        public Builder score(Score score) {
+            this.score = score;
+            return self();
+        }
+
+        public Builder paused(boolean paused) {
+            this.paused = paused;
+            return self();
+        }
+
+        protected Builder self() {
+            return this;
+        }
+
+        public GameState build() {
+            return new GameState(this);
+        }
+    }
 }
